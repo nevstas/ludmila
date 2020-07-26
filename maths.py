96bdc3dc71b6c47104e99ed62975a4e2c57d1845
nevstas <Uf787euyYYwe>
2020-07-26 14:07:04 +0300

    Первый коммит

---
+++ maths.py
@@ -0,0 +1,13 @@
+# -*- coding: windows-1251 -*-
+
+#y = ax + b
+
+
+arr = [1, [1, 2, 3]] #1 параметр y (результат уравнения, второй параметр входящие данные)
+
+
+print (1)
+
+
+
+#c:\\Python38\\python z:\\python\\maths\\maths.py
